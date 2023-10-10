9bf75e944383122b444e78d20a3d3eeb2ff95040
Gisela Benito Izquierdo <gisela.benito@enti.cat>
2023-10-10 18:02:38 +0200

    aaaaaaaaaa

---
+++ ConsoleControl/ConsoleControl/Timer.cpp
@@ -0,0 +1,10 @@
+//#include "Timer.h"
+//
+//void Timer::waitSeconds(int numAnswer)
+//{
+//	std::thread* timepass = new std::thread(&ConsoleControl::Wait,2*numAnswer*1000+2); 
+//
+//	timepass->detach();
+//}
+
+ 
---
+++ ConsoleControl/ConsoleControl/Timer.h
@@ -0,0 +1,15 @@
+//#pragma once
+//#include"ConsoleControl.h"
+//
+//
+//class Timer
+//{
+//private:
+//
+//public: 
+//
+//	Timer() {}; 
+//
+//	static void waitSeconds(int numAnswer);
+//};
+//
