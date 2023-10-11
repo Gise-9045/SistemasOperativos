c2e043a23a01c2f54e68ca65dc304bc464e99529
Gisela Benito Izquierdo <gisela.benito@enti.cat>
2023-10-11 17:07:05 +0200

    aaaaa

--- ConsoleControl/Mini_Practica/TimerManager.h
+++ ConsoleControl/Mini_Practica/TimerManager.h
@@ -1,6 +1,28 @@
 #pragma once
+#include"ConsoleControl.h"
+#include<functional>
+
 class Timer
 {
+public:
+	typedef std::function<void()> OnTimeElapsed; 
+	typedef std::function<bool()> OnTimeElapsedLoop;
+
+public: 
+	static void StartTimer(unsigned long timeRequiredMilisecons ,  OnTimeElapsed onTimeElapse)
+	{
+
+		std::thread* thread = new std::thread([timeRequiredMilisecons, onTimeElapse]()
+		{
+				
+		}
+
+	}
+
+	static void StartTimerLoop(unsigned long timeRequiredMilisecons, OnTimeElapsed onTimeElapse)
+	{
+
+	}
 
 };
 
